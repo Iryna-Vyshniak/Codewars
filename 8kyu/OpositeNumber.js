07e93671571f2d93452581173234fc3c1480bc94
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-06-26 20:17:18 +0300

    feat: Added functions to calculate the opposite of a number for Codewars task

---
+++ 8kyu/OpositeNumber.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+const getOppositeNumber = (num) => 0 - num;
+
+const getOppositeNumber1 = (num) => -num;
